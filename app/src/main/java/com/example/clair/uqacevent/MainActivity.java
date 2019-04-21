bf5f332a2f4177b1f3adce13dd283f4cb89bb5c3
claire-mielcarek <claire.mielcarek@lilo.org>
2019-04-21 10:39:14 -0400

    Menu fonctionnel (fragments)

--- app/src/main/java/com/example/clair/uqacevent/MainActivity.java
+++ app/src/main/java/com/example/clair/uqacevent/MainActivity.java
@@ -1,33 +1,46 @@
 package com.example.clair.uqacevent;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
+    BottomNavigationView navigation;
+    ActionBar actionBar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment f;
             switch (item.getItemId()) {
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
+                    actionBar.setTitle(R.string.title_profile);
+                    f = new ProfileFragment();
+                    openFragment(f);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    actionBar.setTitle(R.string.title_dashboard);
+                    f = new DashboardFragment();
+                    openFragment(f);
                     return true;
                 case R.id.navigation_calendar:
-                    mTextMessage.setText(R.string.title_calendar);
+                    actionBar.setTitle(R.string.title_calendar);
+                    f = new CalendarFragment();
+                    openFragment(f);
                     return true;
                 case R.id.navigation_creation:
-                    mTextMessage.setText(R.string.title_creation);
+                    actionBar.setTitle(R.string.title_creation);
+                    f = new AddEventFragment();
+                    openFragment(f);
                     return true;
             }
             return false;
@@ -38,10 +51,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        actionBar = getSupportActionBar();
+    }
+
+    private void openFragment(Fragment f){
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_container,f);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
